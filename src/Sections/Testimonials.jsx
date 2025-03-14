ccb11b068e067d2b54deb3fc2a3779eb5caaf05c
Ahmed Shehata <Shehataa568@gmail.com>
2025-03-14 02:40:37 +0200

    testimoials

--- src/Sections/Testimonials.jsx
+++ src/Sections/Testimonials.jsx
@@ -3,7 +3,7 @@ import { useCallback } from "react";
 import { useState } from "react";
 
 import { TestimonialsData } from "../SliderData/TestimonialsData";
-import "./Testimonials.css";
+
 
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import { ChevronLeft, ChevronRight } from "@mui/icons-material";
@@ -12,6 +12,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/effect-creative";
+import "./Testimonials.css";
 
 const Testimonials = () => {
   const [isFirst, setIsFirst] = useState(true);
@@ -24,7 +25,7 @@ const Testimonials = () => {
 
   return (
     <div className="testimonials !mt-15 md:!mt-30">
-      <Container>
+      <Container className="!h-[550px]">
         <Typography
           variant="h6"
           sx={{ fontSize: { xs: 12, sm: 15 }, fontWeight: 900 }}
@@ -43,7 +44,7 @@ const Testimonials = () => {
         </Typography>
         <Box component={"div"}>
           <Swiper
-            className="swiper !w-[90%]"
+            className="testimonials-swiper !relative"
             sx={{ boxShadow: 3, borderRadius: "16px" }}
             modules={[Navigation, Pagination, Autoplay]}
             grabCursor={true}
@@ -53,7 +54,11 @@ const Testimonials = () => {
               nextEl: ".custom-next",
               prevEl: ".custom-prev",
             }}
-            pagination={{ clickable: true, dynamicBullets: true }}
+            pagination={{
+              clickable: true,
+              dynamicBullets: true,
+              el: ".custom-pagination",
+            }}
             autoplay={{
               delay: 3000,
               disableOnInteraction: true,
@@ -68,28 +73,24 @@ const Testimonials = () => {
                   fixed
                   sx={{
                     px: { xs: 5, md: 1, lg: 0 },
-                    position: "relative",
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    justifyContent: "center",
                   }}
                 >
                   <Stack
-                    direction="row"
+                    direction={{ xs: "column", md: "row" }}
                     justifyContent={"space-between"}
+                    alignItems={"center"}
+                    gap={{ xs: 4, md: 2 }}
                     className="slider-content text-center md:text-left"
                   >
-                    <Box component={"div"}>
+                    <Box component={"div"} className="md:max-w-[50%] flex flex-col gap-5 justify-center items-center md:block">
                       {/* عنوان السلايد */}
                       <Typography
                         variant="h1"
-                        data-aos="flip-left"
-                        data-aos-delay="300"
                         color="primary"
                         sx={{
-                          fontSize: { xs: 12, sm: 14 },
-                          fontWeight: 700,
+                          fontSize: { xs: 45, sm: 90 },
+                          marginBottom: { xs: 0, sm: 4 },
+
                         }}
                       >
                         {slide?.icon}
@@ -100,10 +101,12 @@ const Testimonials = () => {
                         data-aos="flip-right"
                         data-aos-delay="400"
                         color="text.secondary"
-                        className="xl:max-w-[50%]"
                         sx={{
                           fontSize: { xs: 12, sm: 14 },
-                          fontWeight: 700,
+                          fontWeight: 400,
+                          marginBottom: { xs: 0, sm: 2 },
+                          textAlign: { xs: "center", md: "left" },
+                          width: "100%",
                         }}
                       >
                         {slide.Description}
@@ -113,10 +116,11 @@ const Testimonials = () => {
                         data-aos="flip-right"
                         data-aos-delay="400"
                         color="text.secondary"
-                        className="xl:max-w-[50%]"
+                        className="w-[50%]"
                         sx={{
-                          fontSize: { xs: 12, sm: 14 },
-                          fontWeight: 700,
+                          fontSize: { xs: 12.5, sm: 25 },
+                          textAlign: { xs: "center", md: "left" },
+                          fontWeight: 900,
                         }}
                       >
                         {slide.name}
@@ -125,25 +129,29 @@ const Testimonials = () => {
                         variant="h1"
                         data-aos="flip-right"
                         data-aos-delay="400"
-                        color="text.secondary"
+                        color="primary"
                         className="xl:max-w-[50%]"
+
                         sx={{
-                          fontSize: { xs: 12, sm: 14 },
+                          fontSize: { xs: 9, sm: 18 },
                           fontWeight: 700,
+                          textAlign: { xs: "center", md: "left" },
                         }}
                       >
                         {slide.title}
                       </Typography>
                     </Box>
                     {/* صورة الخلفية */}
-                    <Box
-                      component={"div"}
-                      sx={{ borderRadius: "16px" }}
-                    >
+                    <Box component={"div"}>
                       <img
-                        className="!w-full !h-full !object-cover"
+                        className="!object-cover"
                         src={slide.image}
                         alt=""
+                        style={{
+                          width: "100%",
+                          height: "100%",
+                          objectFit: "cover",
+                        }}
                       />
                     </Box>
                   </Stack>
@@ -151,7 +159,7 @@ const Testimonials = () => {
               </SwiperSlide>
             ))}
             {/* أزرار التنقل */}
-            <div className="">
+            <div className="max-[900px]:hidden">
               <button
                 className={`custom-prev w-[40px] h-[40px] absolute !z-1000 top-1/2 left-0 -translate-y-1/2 cursor-pointer rounded-full bg-[#ff511a] hover:opacity-80 duration-300 ${
                   isFirst ? "opacity-40 pointer-events-none" : "opacity-100"
@@ -160,7 +168,7 @@ const Testimonials = () => {
                 <ChevronLeft />
               </button>
               <button
-                className={`custom-next w-[40px] h-[40px] absolute !z-1000 top-1/2 right-0  -translate-y-1/2 cursor-pointer rounded-full bg-[#ff511a] hover:opacity-80 duration-300 ${
+                className={`custom-next w-[40px] h-[40px] absolute !z-1000 top-1/2 right-0 -translate-y-1/2 cursor-pointer rounded-full bg-[#ff511a] hover:opacity-80 duration-300 ${
                   isLast ? "opacity-40 pointer-events-none" : "opacity-100"
                 }`}
               >
@@ -169,6 +177,7 @@ const Testimonials = () => {
             </div>
           </Swiper>
         </Box>
+        <div className="custom-pagination"></div>
       </Container>
     </div>
   );
