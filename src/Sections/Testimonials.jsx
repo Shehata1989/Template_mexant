626e2cb2965026a8373a974cf89ded24d6d1bb1f
Ahmed Shehata <Shehataa568@gmail.com>
2025-03-18 19:41:31 +0200

    Test

--- src/Sections/Testimonials.jsx
+++ src/Sections/Testimonials.jsx
@@ -36,29 +36,31 @@ const Testimonials = () => {
     return () => {
       setTimeout(() => {
         AOS.refresh(); // تحديث التأثيرات باستمرار
-      }, 100);
+      }, 0);
     };
   }, []);
 
   return (
     <div className="testimonials !mt-15 md:!mt-30 !mb-10 xl:!mb-30 !overflow-hidden">
-      <Container className="">
-        <Typography
-          variant="h6"
-          sx={{ fontSize: { xs: 12, sm: 15 }, fontWeight: 900 }}
-          className="title !mb-2 !uppercase !text-center"
-          color="secondary"
-        >
-          Testimonials
-        </Typography>
-        <Typography
-          variant="h6"
-          sx={{ fontSize: { xs: 20, sm: 36 }, fontWeight: 700 }}
-          className="title !mb-10 !text-center"
-          color="text.secondary"
-        >
-          What They Say
-        </Typography>
+      <Container maxWidth="md" fixed className="">
+        <Box component={"div"}>
+          <Typography
+            variant="h6"
+            sx={{ fontSize: { xs: 12, sm: 15 }, fontWeight: 900 }}
+            className="title !mb-2 !uppercase !text-center"
+            color="secondary"
+          >
+            Testimonials
+          </Typography>
+          <Typography
+            variant="h6"
+            sx={{ fontSize: { xs: 20, sm: 36 }, fontWeight: 700 }}
+            className="title !mb-10 !text-center"
+            color="text.secondary"
+          >
+            What They Say
+          </Typography>
+        </Box>
         <Box component={"div"} sx={{ position: "relative" }}>
           <Swiper
             className="testimonials-swiper"
@@ -85,26 +87,20 @@ const Testimonials = () => {
           >
             {TestimonialsData.map((slide, index) => (
               <SwiperSlide key={`${index}-${new Date().getTime()}`}>
-                <Container
-                  maxWidth="md"
-                  fixed
-                  sx={{
-                    px: { xs: 5, md: 1, lg: 0 },
-                  }}
-                >
+                <Container maxWidth="md" fixed>
                   <Stack
+                    className="slider-content"
                     sx={{
                       boxShadow: 3,
                       borderRadius: "16px",
-                      paddingLeft: { xs: 0, md: 4 },
                     }}
                     direction={{ xs: "column", md: "row" }}
                     justifyContent={"space-between"}
                     alignItems={"center"}
                     gap={{ xs: 4, md: 2 }}
-                    className="slider-content text-center md:text-left"
                   >
                     <Box
+                    sx={{ width: { xs: "100%", md: "50%" }, paddingLeft: { xs: 0, md: 4 } }}
                       component={"div"}
                       className="md:max-w-[50%] flex flex-col gap-5 justify-center items-center md:block"
                     >
@@ -128,7 +124,7 @@ const Testimonials = () => {
                           fontWeight: 300,
                           marginBottom: { xs: 0, sm: 2 },
                           textAlign: { xs: "center", md: "left" },
-                          width: "100%",
+                          px: { xs: 2, md: 0 },
                         }}
                       >
                         {slide.Description}
@@ -160,14 +156,18 @@ const Testimonials = () => {
                         {slide.title}
                       </Typography>
                     </Box>
-                    <Box component={"div"}>
+                    <Box
+                      component={"div"}
+                      sx={{ width: { xs: "100%", md: "auto" }, height: "100%" }}
+                    >
                       <img
                         src={slide.image}
                         alt=""
                         style={{
-                          width: "300px",
+                          width: "100%",
                           height: "100%",
                           objectFit: "cover",
+                          borderRadius: "16px",
                         }}
                       />
                     </Box>
@@ -175,24 +175,24 @@ const Testimonials = () => {
                 </Container>
               </SwiperSlide>
             ))}
-            {/* أزرار التنقل */}
-            <div className="max-[900px]:hidden">
-              <button
-                className={`custom-prev w-[40px] h-[40px] absolute !z-1000 top-1/2 left-0 -translate-y-1/2 cursor-pointer rounded-full bg-[#ff511a] hover:opacity-80 duration-300 ${
-                  isFirst ? "opacity-40 pointer-events-none" : "opacity-100"
-                }`}
-              >
-                <ChevronLeft />
-              </button>
-              <button
-                className={`custom-next w-[40px] h-[40px] absolute !z-1000 top-1/2 right-0 -translate-y-1/2 cursor-pointer rounded-full bg-[#ff511a] hover:opacity-80 duration-300 ${
-                  isLast ? "opacity-40 pointer-events-none" : "opacity-100"
-                }`}
-              >
-                <ChevronRight />
-              </button>
-            </div>
           </Swiper>
+          {/* أزرار التنقل */}
+          <div className="max-[900px]:hidden">
+            <button
+              className={`custom-prev w-[40px] h-[40px] absolute !z-1000 top-[42%] left-0 -translate-y-1/2 cursor-pointer rounded-full bg-[#ff511a] hover:opacity-80 duration-300 ${
+                isFirst ? "opacity-40 pointer-events-none" : "opacity-100"
+              }`}
+            >
+              <ChevronLeft />
+            </button>
+            <button
+              className={`custom-next w-[40px] h-[40px] absolute !z-1000 top-1/2 right-0 -translate-y-1/2 cursor-pointer rounded-full bg-[#ff511a] hover:opacity-80 duration-300 ${
+                isLast ? "opacity-40 pointer-events-none" : "opacity-100"
+              }`}
+            >
+              <ChevronRight />
+            </button>
+          </div>
           {/* الـ Pagination متمركز داخل الـ Container */}
           <div className="custom-pagination absolute bottom-0 left-0 right-0 flex justify-center items-center !mt-5 md:!mt-10"></div>
         </Box>
